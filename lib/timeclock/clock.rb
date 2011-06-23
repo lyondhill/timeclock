8c6047f2ee5a689c7debde5f4f3fbb1e81e2b7c0
Lyon <lyon@delorum.com>
2011-06-23 00:08:35 -0600

    add in the awesome helper functions so clay can build a prettier view in the email and in the other page.

--- lib/timeclock/clock.rb
+++ lib/timeclock/clock.rb
@@ -51,27 +51,53 @@ module Timeclock
       puts "clocking out at #{Time.now.to_s}"
     end
 
+    def total
+      total = 0.0
+      collect.each {|value|total += value[:hours]}
+        total
+    end
+
+    def daily_log
+      collection = collect
+      new_hash = {}
+      time_array = []
+      collection.each do |element|
+        day = Time.parse(element[:clock_in]).day
+        if new_hash[day]
+          new_hash[day][:total] += element[:hours]
+          new_hash[day][:log] << {:in => element[:clock_in], :out => element[:clock_in]}
+        else
+          new_hash[day] = {}
+          new_hash[day][:total] = element[:hours]
+          new_hash[day][:log] = [{:in => element[:clock_in], :out => element[:clock_in]}]
+        end
+      end
+      new_hash[:total] = total
+      new_hash
+    end
+
     def collect
       string = get_log_string
       array = string.split("\n")
       rtn = []
-      until array.empty?
-        clock_in = array.shift
-        clock_out = array.shift
-        if clock_in && clock_out
+      if array.size.even?
+        until array.empty?
+          clock_in = array.shift
+          clock_out = array.shift
           in_array = clock_in.split(" ")
           out_array = clock_out.split(" ")
           in_time = Time.parse("#{in_array[2]} #{in_array[3]} #{in_array[4]}")
-          out_time = Time.parse("#{out_array[2]} #{out_array[3]} #{out_array[4]}")
+          out_time = Time.parse("#{out_array[1]} #{out_array[2]} #{out_array[3]}")
           hash = {}
           hash[:hours] = (out_time - in_time) / 3600 #convert to hours
-          hash[:clock_in] = in_time.to_s 
-          hash[:clock_out] = out_time.to_s
+          hash[:clock_in] = in_time.ctime 
+          hash[:clock_out] = out_time.ctime
           hash[:project] = in_array[1]
           rtn << hash
-        else
-          puts "You do not have an even number of clock in's and out's"
         end
+      else
+        puts "You do not have an even number of clock in's and out's"
+        exit(1)
       end
       rtn
     end
